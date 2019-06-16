e9e94323a364b3e8115558510c5db1c0518e61c3
李云鹏 <flowerinhouse@163.com>
2019-06-17 06:34:22 +0800

    完成公告资讯版块

---
+++ pages/stock/announcement.js
@@ -0,0 +1,242 @@
+// pages/stock/announcement.js
+const economic = require('../../services/economic.service.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    announcements: [],
+    dividends: [],
+    spo: [],
+    rationed: [],
+    raising: [],
+    displayedTags: []
+  },
+
+  stockCode: '',
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log('======== 	onLoad	========')
+    this.stockCode = options.code;
+    this.setData({
+      displayedTags: [{
+        name: '公告',
+        icon: '/icons/stock/_gonggao.png',
+        disabledIcon: '/icons/stock/_gonggao_grey.png',
+        apiFunc: this.getStockAnnouncement,
+        enable: true
+      }, {
+        name: '分红',
+        icon: '/icons/stock/_fenhong.png',
+        disabledIcon: '/icons/stock/_fenhong_grey.png',
+        apiFunc: this.getStockDividends,
+        enable: false
+      }, {
+        name: '增发',
+        icon: '/icons/stock/_zengfa.png',
+        disabledIcon: '/icons/stock/_zengfa_grey.png',
+        apiFunc: this.getStockSecondaryPublicOffering,
+        enable: false
+      }, {
+        name: '配股',
+        icon: '/icons/stock/_peigu.png',
+        disabledIcon: '/icons/stock/_peigu_grey.png',
+        apiFunc: this.getStockRationedShare,
+        enable: false
+      }, {
+        name: '募集',
+        icon: '/icons/stock/_muji.png',
+        disabledIcon: '/icons/stock/_muji_grey.png',
+        apiFunc: this.getStockRaisingFundUsage,
+        enable: false
+      }]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    console.log('======== 	onReady	========')
+    this.paintAnnouncement({
+      code: this.stockCode
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  /**
+   * 	点击公告查看详情
+   */
+  onAnnouncementClicked: function(evt) {
+    console.log(evt);
+  },
+
+  /**
+   * 	获取上市公司公告资讯
+   */
+  getStockAnnouncement: function(request) {
+    economic.getStockAnnouncement(request.code)
+      .then(res => {
+        this.resultHandler('announcements', res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+  /**
+   * 	获取上市公司分红转增
+   */
+  getStockDividends: function(request) {
+    economic.getStockDividends(request.code)
+      .then(res => {
+        this.resultHandler('dividends', res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+  /**
+   * 	公司增发股票实施方案
+   */
+  getStockSecondaryPublicOffering: function(request) {
+    economic.getStockSecondaryPublicOffering(request.code)
+      .then(res => {
+        this.resultHandler('spo', res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+  /**
+   * 	公司配股实施方案
+   */
+  getStockRationedShare: function(request) {
+    economic.getStockRationedShare(request.code)
+      .then(res => {
+        this.resultHandler('rationed', res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+  /**
+   * 	募集资金投资项目计划
+   */
+  getStockRaisingFundUsage: function(request) {
+    economic.getStockRaisingFundUsage(request.code)
+      .then(res => {
+        this.resultHandler('raising', res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+  resultHandler: function(key, res) {
+    console.log(res);
+    if (res.statusCode === 200) {
+      const data = JSON.parse(res.data);
+      if (data.hasOwnProperty('resultcode') && data.resultcode === 200) {
+        if (data.records.length > 0) {
+          let value = {};
+          value[key] = data.records;
+          this.setData(value);
+        } else { //  返回的记录数组为空
+          wx.showToast({
+            title: '不存在相关记录',
+            icon: 'none'
+          })
+        }
+      } else { //  API调用失败
+        wx.showToast({
+          title: data.resultmsg,
+          icon: 'none'
+        })
+      }
+    } else { //  网络出错
+      wx.showToast({
+        title: '服务器开小差~~',
+        icon: 'none'
+      })
+    }
+  },
+
+  /**
+   *  绘制页面
+   */
+  paintAnnouncement: function(request) {
+    this.data.displayedTags.map(tag => {
+      if (tag.enable) {
+        tag.apiFunc(request);
+        wx.setNavigationBarTitle({
+          title: tag.name
+        })
+      }
+    })
+  },
+
+  /**
+   *  选择类别
+   */
+  onTagClicked: function(evt) {
+    this.setData({
+      displayedTags: this.data.displayedTags.map(item => {
+        item.enable = (item.name === evt.currentTarget.dataset.name) ? true : false;
+        return item;
+      })
+    });
+    this.paintAnnouncement({
+      code: this.stockCode
+    });
+  }
+})
--- pages/stock/pledge.js
+++ pages/stock/pledge.js
@@ -2,94 +2,157 @@
 const economic = require('../../services/economic.service.js');
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        pledges: []
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pledges: [],
+    displayedTags: []
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        console.log(options);
-        this.getStockSharePledge({
-            code: options.code
-        });
-    },
+  stockCode: '',
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log(options);
+    this.stockCode = options.code;
+    this.setData({
+      displayedTags: [{
+        name: '持股',
+        icon: '/icons/stock/_gubenjiegou.png',
+        disabledIcon: '/icons/stock/_gubenjiegou_grey.png',
+        // apiFunc: this.getStockAnnouncement,
+        enable: true
+      }, {
+        name: '质押',
+        icon: '/icons/stock/_guquanchuzhi.png',
+        disabledIcon: '/icons/stock/_guquanchuzhi_grey.png',
+        apiFunc: this.getStockSharePledge,
+        enable: false
+      }, {
+        name: '冻结',
+        icon: '/icons/stock/_jingyingyichang.png',
+        disabledIcon: '/icons/stock/_jingyingyichang_grey.png',
+        // apiFunc: this.getStockSecondaryPublicOffering,
+        enable: false
+      }, {
+        name: '解禁',
+        icon: '/icons/stock/_huanbenqueren.png',
+        disabledIcon: '/icons/stock/_huanbenqueren_grey.png',
+        // apiFunc: this.getStockRationedShare,
+        enable: false
+      }]
+    })
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    console.log('======== 	onReady	========')
+    this.paint({
+      code: this.stockCode
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    },
+  },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
-    },
+  },
 
-    /**
-     * 	获取公司股权质押
-     */
-    getStockSharePledge: function(request) {
-        const that = this;
-        economic.getStockSharePledge(request.code)
-            .then(res => {
-                console.log(res);
-                const data = JSON.parse(res.data);
-                if (data.hasOwnProperty('resultcode') && data.resultcode === 200 && data.records.length > 0) {
-                    let index = 0;
-                    this.data.pledges = data.records.map(record => {
-                        record.index = ++index;
-                        return record;
-                    })
-                    this.setData({
-                        pledges: this.data.pledges
-                    })
-                }
-            })
-            .catch(err => {
-                console.error(err);
-            })
-    },
+  /**
+   * 	获取公司股权质押
+   */
+  getStockSharePledge: function(request) {
+    const that = this;
+    economic.getStockSharePledge(request.code)
+      .then(res => {
+        this.resultHandler('pledges', res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+  resultHandler: function(key, res) {
+    console.log(res);
+    if (res.statusCode === 200) {
+      const data = JSON.parse(res.data);
+      if (data.hasOwnProperty('resultcode') && data.resultcode === 200) {
+        if (data.records.length > 0) {
+          let value = {};
+          value[key] = data.records;
+          this.setData(value);
+        } else { //  返回的记录数组为空
+          wx.showToast({
+            title: '不存在相关记录',
+            icon: 'none'
+          })
+        }
+      } else { //  API调用失败
+        wx.showToast({
+          title: data.resultmsg,
+          icon: 'none'
+        })
+      }
+    } else { //  网络出错
+      wx.showToast({
+        title: '服务器开小差~~',
+        icon: 'none'
+      })
+    }
+  },
+
+  /**
+   *  绘制页面
+   */
+  paint: function(request) {
+    this.data.displayedTags.map(tag => {
+      if (tag.enable && tag.apiFunc) {
+        tag.apiFunc(request);
+        wx.setNavigationBarTitle({
+          title: tag.name
+        })
+      }
+    })
+  }
 })
