c54ae6b5feb10258a6f887064e75c1e67fdd91eb
thinmelon <flowerinhouse@163.com>
2019-07-05 17:09:54 +0800

    完成招投标模块的显示页面
    加入fundebug监控小程序
    不能访问巨潮资讯的接口，调查下是什么原因

--- app.js
+++ app.js
@@ -1,113 +1,117 @@
 //app.js
 const __WX_API_PROMISE__ = require('./utils/wx.api.promise.js');
+const __FUN_DEBUG__ = require('./lib/fundebug.1.3.1.min.js');
+__FUN_DEBUG__.init({
+    apikey: '5deb327332863618ddb0691554c9febfc1ae98db0266ba7620a33d97e258dbd3'
+})
 
 App({
-  environment: 'PRODUCTION', //	测试环境：DEBUG		生产环境：PRODUCTION
-  timeOut: 1000, //	超时重试：1000毫秒
-  /**
-   *  股票市场
-   *  
-   */
-  markets: [{
-    name: 'A股', //  名称
-    abbreviation: 'hs', //  缩写
-    icon: '/icons/stock/China.png', //  启用图标
-    disabledIcon: '/icons/stock/Grey.png', //  禁用图标
+    environment: 'PRODUCTION', //	测试环境：DEBUG		生产环境：PRODUCTION
+    timeOut: 1000, //	超时重试：1000毫秒
     /**
-     *  点击股票弹出的ActionSheet显示内容荐
+     *  股票市场
+     *  
      */
-    properties: [{
-      tapIndex: 0, //  索引
-      name: '公司资讯', //  项目名
-      url: '/pages/stock/announcement?market=hs' //  跳转页面路径 
+    markets: [{
+        name: 'A股', //  名称
+        abbreviation: 'hs', //  缩写
+        icon: '/icons/stock/China.png', //  启用图标
+        disabledIcon: '/icons/stock/Grey.png', //  禁用图标
+        /**
+         *  点击股票弹出的ActionSheet显示内容荐
+         */
+        properties: [{
+            tapIndex: 0, //  索引
+            name: '公司资讯', //  项目名
+            url: '/pages/stock/announcement?market=hs' //  跳转页面路径 
+        }, {
+            tapIndex: 1,
+            name: '股权结构',
+            url: '/pages/stock/pledge?market=hs'
+        }, {
+            tapIndex: 2,
+            name: '资产负债',
+            url: '/pages/stock/balance?market=hs'
+        }, {
+            tapIndex: 3,
+            name: 'K线',
+            url: '/pages/stock/k.line?market=hs'
+        }],
+        enable: true
     }, {
-      tapIndex: 1,
-      name: '股权结构',
-      url: '/pages/stock/pledge?market=hs'
+        name: '港股',
+        abbreviation: 'hk',
+        icon: '/icons/stock/HongKong.png',
+        disabledIcon: '/icons/stock/Grey.png',
+        properties: [{
+            tapIndex: 0,
+            name: 'K线',
+            url: '/pages/stock/k.line?market=hk'
+        }],
+        enable: false
     }, {
-      tapIndex: 2,
-      name: '资产负债',
-      url: '/pages/stock/balance?market=hs'
+        name: '美股',
+        abbreviation: 'usa',
+        icon: '/icons/stock/UnitedStates.png',
+        disabledIcon: '/icons/stock/Grey.png',
+        properties: [{
+            tapIndex: 0,
+            name: 'K线',
+            url: '/pages/stock/k.line?market=usa'
+        }],
+        enable: false
+    }],
+    /**
+     *  类别标签 
+     * 
+     */
+    tags: [{
+        name: '中餐',
+        icon: '/icons/food/tags/zhongcan.png',
+        disabledIcon: '/icons/food/tags/zhongcan_grey.png',
+        category: ['中餐'],
+        enable: true
     }, {
-      tapIndex: 3,
-      name: 'K线',
-      url: '/pages/stock/k.line?market=hs'
-    }],
-    enable: true
-  }, {
-    name: '港股',
-    abbreviation: 'hk',
-    icon: '/icons/stock/HongKong.png',
-    disabledIcon: '/icons/stock/Grey.png',
-    properties: [{
-      tapIndex: 0,
-      name: 'K线',
-      url: '/pages/stock/k.line?market=hk'
-    }],
-    enable: false
-  }, {
-    name: '美股',
-    abbreviation: 'usa',
-    icon: '/icons/stock/UnitedStates.png',
-    disabledIcon: '/icons/stock/Grey.png',
-    properties: [{
-      tapIndex: 0,
-      name: 'K线',
-      url: '/pages/stock/k.line?market=usa'
-    }],
-    enable: false
-  }],
-  /**
-   *  类别标签 
-   * 
-   */
-  tags: [{
-    name: '中餐',
-    icon: '/icons/food/tags/zhongcan.png',
-    disabledIcon: '/icons/food/tags/zhongcan_grey.png',
-    category: ['中餐'],
-    enable: true
-  }, {
-    name: '烧烤串串',
-    icon: '/icons/food/tags/shaokao.png',
-    disabledIcon: '/icons/food/tags/shaokao_grey.png',
-    category: ['烧烤', '串串'],
-    enable: false
-  }, {
-    name: '火锅自助',
-    icon: '/icons/food/tags/huoguo.png',
-    disabledIcon: '/icons/food/tags/huoguo_grey.png',
-    category: ['火锅', '自助'],
-    enable: false
-  }, {
-    name: '日韩料理',
-    icon: '/icons/food/tags/riliao.png',
-    disabledIcon: '/icons/food/tags/riliao_grey.png',
-    category: ['日料', '韩餐'],
-    enable: false
-  }, {
-    name: '甜点茶饮',
-    icon: '/icons/food/tags/tiandian.png',
-    disabledIcon: '/icons/food/tags/tiandian_grey.png',
-    category: ['甜点', '茶饮'],
-    enable: false
-  }, { //	餐馆标签
-    name: '小吃简餐',
-    icon: '/icons/food/tags/xiaochi.png',
-    disabledIcon: '/icons/food/tags/xiaochi_grey.png',
-    category: ['小吃', '简餐'],
-    enable: false
-  }, {
-    name: '西餐',
-    icon: '/icons/food/tags/xican.png',
-    disabledIcon: '/icons/food/tags/xican_grey.png',
-    category: ['西餐'],
-    enable: false
-  }, {
-    name: '酒吧',
-    icon: '/icons/food/tags/jiuba.png',
-    disabledIcon: '/icons/food/tags/jiuba_grey.png',
-    category: ['酒吧'],
-    enable: false
-  }]
+        name: '烧烤串串',
+        icon: '/icons/food/tags/shaokao.png',
+        disabledIcon: '/icons/food/tags/shaokao_grey.png',
+        category: ['烧烤', '串串'],
+        enable: false
+    }, {
+        name: '火锅自助',
+        icon: '/icons/food/tags/huoguo.png',
+        disabledIcon: '/icons/food/tags/huoguo_grey.png',
+        category: ['火锅', '自助'],
+        enable: false
+    }, {
+        name: '日韩料理',
+        icon: '/icons/food/tags/riliao.png',
+        disabledIcon: '/icons/food/tags/riliao_grey.png',
+        category: ['日料', '韩餐'],
+        enable: false
+    }, {
+        name: '甜点茶饮',
+        icon: '/icons/food/tags/tiandian.png',
+        disabledIcon: '/icons/food/tags/tiandian_grey.png',
+        category: ['甜点', '茶饮'],
+        enable: false
+    }, { //	餐馆标签
+        name: '小吃简餐',
+        icon: '/icons/food/tags/xiaochi.png',
+        disabledIcon: '/icons/food/tags/xiaochi_grey.png',
+        category: ['小吃', '简餐'],
+        enable: false
+    }, {
+        name: '西餐',
+        icon: '/icons/food/tags/xican.png',
+        disabledIcon: '/icons/food/tags/xican_grey.png',
+        category: ['西餐'],
+        enable: false
+    }, {
+        name: '酒吧',
+        icon: '/icons/food/tags/jiuba.png',
+        disabledIcon: '/icons/food/tags/jiuba_grey.png',
+        category: ['酒吧'],
+        enable: false
+    }]
 })
--- pages/tendering/bid.js
+++ pages/tendering/bid.js
@@ -7,23 +7,41 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        companies: [
+            '集团',
+            '平潭',
+            '福州',
+            '厦门',
+            '泉州',
+            '莆田',
+            '三明',
+            '南平',
+            '龙岩',
+            '漳州',
+            '宁德'
+        ],
+        bidAmount: [
+            '不限',
+            '小于100万',
+            '100万至500万',
+            '500万至1000万',
+            '1000万以上'
+        ],
+        tenderings: [],
+        companyPickerName: '福建广电网络集团',
+        bidAmountPickerName: '不限'
     },
 
     offset: 0, //	偏移量
     itemsPerTime: 10, //	每次获取数量
+    minBidAmount: 0, //	最少中标金额
+    maxBidAmount: 100000000, //	最多中标金额
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        this.searchTenderings({
-            name: '福建广电网络集团股份有限公司莆田分公司',
-            min: 50,
-            max: 100,
-            skip: this.offset,
-            limit: this.itemsPerTime
-        });
+        this.showTenderings();
     },
 
     /**
@@ -76,17 +94,138 @@ Page({
     },
 
     /**
-     * 	获取餐馆列表
+     *  滚动至底部，获取餐馆列表
+     */
+    onScrollToButtom: function() {
+        console.log('======== onScrollToButtom ========')
+        this.showTenderings();
+    },
+
+    /**
+     * 	显示中标结果
+     */
+    showTenderings: function() {
+        this.searchTenderings({
+            name: this.data.companyPickerName,
+            min: this.minBidAmount,
+            max: this.maxBidAmount,
+            skip: this.offset,
+            limit: this.itemsPerTime
+        });
+    },
+
+    /**
+     * 	搜索中标结果
      */
     searchTenderings: function(request) {
         console.log('>>>>>>>>>>>>> searchTenderings');
         __ECONOMIC__.searchTenderings(request)
             .then(res => {
-                console.log(res.data);
+                console.log(res);
+                if (res.statusCode === 200) {
+                    if (res.data.code === 0 && res.data.data.tenderings.length > 0) {
+                        this.offset += res.data.data.tenderings.length; //	下次获取数据时起点位置
+                        this.setData({
+                            tenderings: this.data.tenderings.concat(res.data.data.tenderings.map(item => {
+                                if (item.announcementDate) {
+                                    item.announcementDate = item.announcementDate.substr(0, 10)
+                                }
+                                if (item.resultDate) {
+                                    item.resultDate = item.resultDate.substr(0, 10)
+                                }
+                                item.supplier = item.supplier.map(value => {
+                                    value.price = value.price.toFixed(2);
+                                    return value;
+                                })
+                                return item;
+                            }))
+                        });
+                    }
+                } else if (res.statusCode === 503) {
+                    setTimeout(() => {
+                        this.searchTenderings(request);
+                    }, getApp().timeOut); //	如果是503错误（服务器在忙），1秒后发起重试
+                } else { //  网络出错
+                    wx.showToast({
+                        title: '服务器开小差~~',
+                        icon: 'none'
+                    })
+                }
 
             })
             .catch(err => {
                 console.error(err);
             })
     },
+
+    /**
+     * 		公司Picker Change
+     */
+    bindCompanyPickerChange: function(evt) {
+        const index = parseInt(evt.detail.value);
+        let companyName = '';
+
+        if (index === 0) {
+            companyName = '福建广电网络集团';
+        } else if (index === 1) {
+            companyName = '福建广电网络集团股份有限公司平潭综合实验区分公司';
+        } else if (index > 0) {
+            companyName = '福建广电网络集团股份有限公司' + this.data.companies[index] + '分公司';
+        }
+
+        this.setData({
+            tenderings: [], //	清空结果集
+            companyPickerName: companyName //	设置Picker内容
+        });
+        this.offset = 0; // 重置偏移量
+        this.showTenderings();
+    },
+
+    /**
+     * 		中标金额Picker Change
+     */
+    bindBidAmountPickerChange: function(evt) {
+        console.log(evt);
+        const index = parseInt(evt.detail.value);
+        switch (index) {
+            case 1: //	'小于100万',
+                this.minBidAmount = 0;
+                this.maxBidAmount = 100;
+                break;
+            case 2: //	'100万至500万',
+                this.minBidAmount = 100;
+                this.maxBidAmount = 500;
+                break;
+            case 3: //	'500万至1000万',
+                this.minBidAmount = 500;
+                this.maxBidAmount = 1000;
+                break;
+            case 4: //	'1000万以上'
+                this.minBidAmount = 1000;
+                this.maxBidAmount = 100000000;
+                break;
+            default: //	'不限',
+                this.minBidAmount = 0;
+                this.maxBidAmount = 100000000;
+                break;
+        }
+
+        this.setData({
+            tenderings: [], //	清空结果集
+            bidAmountPickerName: this.data.bidAmount[index] //	设置Picker内容
+        });
+        this.offset = 0; // 重置偏移量
+        this.showTenderings();
+    },
+
+    /**
+     *  跳转至微信公众号文章链接
+     */
+    catchTapTendering: function(evt) {
+        console.log(evt);
+        const url = evt.currentTarget.dataset.tendering.demo;
+        wx.navigateTo({
+            url: '/pages/food/article?collection=' + url.substr('http://oss.pusudo.cn/life/'.length, 32) + '&source=' + evt.currentTarget.dataset.tendering.number
+        })
+    }
 })
